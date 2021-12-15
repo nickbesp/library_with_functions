1fba04d77a80a7e86e13e0faee1d8ee0b88d8661
Bespyatuh Nikita <78490935+BespyatihNikita@users.noreply.github.com>
2021-12-15 21:07:21 +0500

    Update library.py

--- library.py
+++ library.py
@@ -1,18 +1,18 @@
-def large(arr): 
+def large(arr): #max() 
     max_ = arr[0]
     for ele in arr:
         if ele > max_:
             max_ = ele
     return max_
 
-def in_dec_num_syst(x, syst):
+def in_dec_num_syst(x, syst): #перевод в десятичную систему счисления
     summa = 0
     x = str(x)
     for i in range(len(x)):
         summa += int(x[i]) * syst ** (len(x) - i - 1)
     return summa
 
-def from_dec_num_syst(x, syst):
+def from_dec_num_syst(x, syst): #перевод из десятичной системы счисления
     s = ''
     while x // syst != 0:
         s += str(x % syst)
@@ -20,7 +20,7 @@ def from_dec_num_syst(x, syst):
     s += str(x)
     return s[::-1]
 
-def amount_words(s):
+def amount_words(s): #количество слов в строке :)
     k = 0
     s = ' ' + s
     for i in range(len(s) - 1):
