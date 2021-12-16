420f9cac9ed8fdc60f53698c17a0d6e05d66bb04
Bespyatuh Nikita <78490935+BespyatihNikita@users.noreply.github.com>
2021-12-16 22:53:00 +0500

    Update library.py

--- library.py
+++ library.py
@@ -28,15 +28,27 @@ def amount_words(s): #количество слов в строке :)
             k += 1
     return k
 
-def matrix_x_matrix(m1, m2): #Умножение двух матриц (для 10 класса)
+def matrix_x_matrix(m1, m2): #Умножение двух матриц (для 10 класса) m1 * m2
     r = []
     m = []
-    for i in range(len(m1)):
-        for j in range(len(m2[0])):
-            sums = 0
-            for k in range(len(m2)):
-                sums += (m1[i][k] * m2[k][j])
-            r.append(sums)
-        m.append(r)
-        r = []
+    if len(m1) == len(m2[0]):
+        for i in range(len(m1)):
+            for j in range(len(m2[0])):
+                sums = 0
+                for k in range(len(m2)):
+                    sums += (m1[i][k] * m2[k][j])
+                r.append(sums)
+            m.append(r)
+            r = []
+    else:
+        return 'Error! This version does not work with matrices in which the number of rows of the first does not match the number of columns of the second! Try to find another algorithm or write your own.'
     return m
+
+def amount_num_in_matrix(mat, num): #Количество чисел num в двумерном массиве mat
+    k = 0
+    n = len(mat)
+    for i in range(n):
+        for j in range(len(mat[i])): 
+            if mat[i][j] == num:
+                k += 1   
+    return k    
