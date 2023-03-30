4d39f146fffd443f8d6232f5b30c31d20ef5c3e1
Bespyatykh Nikita <78490935+nickbesp@users.noreply.github.com>
2023-03-30 19:28:13 +0500

    Update library.py

--- library.py
+++ library.py
@@ -139,3 +139,58 @@ def multMatNum(matrix, num): #Умножение матрицы на число
             row.append(matrix[i][j] * num)
         result.append(row)
     return result
+
+#Транспонирование матриц
+def transpos(matrix):
+    #Создание пустой матрицы jxi (изначально у нас ixj)
+    result = list()
+    for i in range(len(matrix[0])):
+        row = list()
+        for j in range(len(matrix)):
+            row.append(0)
+        result.append(row)
+        
+    for i in range(len(matrix)):
+        for j in range(len(matrix[0])):
+            result[j][i] = matrix[i][j]
+    return result
+
+#Определитель матрицы
+def det(matrix):
+
+    if len(matrix) != len(matrix[0]):
+        raise ValueError('The number of the rows of the matrix should be equal with the number of the columns!')
+
+    if len(matrix) == 2:
+        return minor(matrix)
+    
+    #Правило Лапласа
+    summ = 0
+    for j in range(len(matrix)):
+        elem = matrix[0][j]
+        summ += elem * adding(matrix, 0, j)
+
+    return summ
+
+#Алгоритмическое дополнение элемента матрицы
+def adding(matrix, i, j):
+    if len(matrix) == 2:
+        return(minor(matrix) * (-1)**(i + j))
+
+    #Создание матрицы на порядок меньше, для которой будет считаться минор
+    new_mat = list()
+    for q in range(len(matrix[0])):
+        if q == i:
+            continue
+        row = list()
+        for w in range(len(matrix)):
+            if w == j:
+                continue
+            row.append(matrix[q][w])
+        new_mat.append(row)
+
+    return det(new_mat) * (-1)**(i + j)
+
+#Минор к элементу матрицы 2x2
+def minor(matrix):
+    return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
